5d468d328396aa6acc69a0efe424f92a36ca1f15
Pedro Reis <dropecosta@gmail.com>
2025-06-18 10:03:47 +0100

    Loading data with thunks

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://5lp3xl1v-3000.uks1.devtunnels.ms/',
+        changeOrigin: true,
+      }
+    }
+  }
 })
